868e434d816b45741c2926c1e095f2b858d2705a
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-17 14:13:02 -0400

    more code added to js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,53 +1,105 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-
+//-----------------------------------------------------------//
 // Create the Connection to the DB //
+//-----------------------------------------------------------//
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    
+
     user: "root",
     password: "root",
     database: "bamazon_DB"
 });
 
 // Connect to SQL server and SQL DB
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
-    
+
     // run start function after connection is made to prompt user
     showProducts();
 });
+//-----------------------------------------------------------//
+// end Connection to the DB //
+//-----------------------------------------------------------//
 
+
+
+//-----------------------------------------------------------//
+//function to display items for sale on the console
+//-----------------------------------------------------------//
 function showProducts() {
-    connection.query('SELECT * FROM products', function(err, res){
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
 
         console.log('');
         console.log('-------------Current Inventory---------------');
         console.log('');
-    
-        
+
+
         for (var i = 0; i < res.length; i++) {
             console.log('Item ID: ' + res[i].id + '      Product: ' + res[i].product + '      Department: ' + res[i].department);
             console.log('Price: ' + res[i].price + '      Quanity Left: ' + res[i].quanity);
             console.log(' ');
-            console.log(' ');
+
         }
-        
+        //call function to start the user prompt and shopping//
+        start();
     })
 }
 
+//-----------------------------------------------------------//
+// end of function to display items for sale on the console
+//-----------------------------------------------------------//
 
 
-
-
-
+//-----------------------------------------------------------//
 // function to prompt user  on what they would like to do
-function start(){
+//-----------------------------------------------------------//
+function start() {
     inquirer
-    .prompt({
-        
-    })
-}
+        .prompt([
+            {
+                name: 'selectId',
+                type: 'input',
+                message: 'Enter ITEM ID for product you wish to purchase:',
+
+        },
+
+            {
+                name: 'quanity',
+                type: 'input',
+                message: 'How many would you like?'
+            },
+          
+          ]).then(function (answers) {
+                // get the information of the chosen item, set input to variables, pass variables as Parameters
+                var quanityBought = answers.quanity;
+                var itemBought = answers.selectId;
+
+                boughtFromDB(quanityBought, itemBought);
+          
+        });
+};
+    //
+
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              

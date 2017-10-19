94f8f1725a015ef8f7f20dec142b12b24c0d0954
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-19 15:37:13 -0400

    working on the manager functionality, have add to stock working

--- bamazonManager.js
+++ bamazonManager.js
@@ -60,93 +60,139 @@ function showProducts() {
 // function to prompt user  on what they would like to do
 //-----------------------------------------------------------//
 function manageStore() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw console.log("connection error:" + err);
-    inquirer
-        .prompt([
-                {
-                    name: 'selectId',
-                    type: 'input',
-                    message: 'Choose an option below to manage inventory:',
-                    choices: ["Add inventory", "Add a new product", "Remove a product"]
-                    
+    connection.query("SELECT * FROM products", function (err, res) {
+            if (err) throw console.log("connection error:" + err);
+            inquirer
+                .prompt([
+                    {
+                        name: 'selectId',
+                        type: 'list',
+                        message: 'Choose an option below to manage inventory:',
+                        choices: ["Add inventory", "Add a new product", "Remove a product", "Quit"]
+
 
         }
 
-               
-            ]).then (function (answers) {
-            var query = "SELECT * FROM products WHERE ?";
-            connection.query(query, {
-                id: answers.selectId
-            }, function (err, res) {
+
+            ]).then(function (answers) {
+                    if (answers.selectId === "Add inventory") {
+                        addInventory()
+                    } else if (answers.selectId === "Add a new product") {
+                        newProduct()
+                    } else if (answers.selectId === "Remove a product") {
+                        deleteProduct()
+                    } else if (answers.selectId === "Quit") {
+                        quit()
+                    } else {
+                        console.log("Please choose one of the options");
+                    }
+                });
 
 
-                // get the information of the chosen item, set input to variables, pass variables as Parameters
+            function addInventory() {
+              inquirer.prompt([
 
-                var inStock = res[0].quanity;
-                var itemBought = answers.amountBought;
+        {
+            name: "ID",
+            type: "input",
+            message: "What is the item number you wish to restock?"
+        }, {
+            name: 'Quantity',
+            type: 'input',
+            message: "How many would you like to add?"
+        },
 
-                if (inStock >= itemBought) {
-                    var leftInStock = inStock - itemBought;
-                    
-                    ///  testing app to check values of variables /////
-                    ///////////////////////////////////////////////////////////////////////////
-                    
-//                    
-//                    console.log(leftInStock + "  amount left in stock ******");
-//                    console.log(itemBought + "  amount bought");
-//                    console.log(res[0].price + "   price");
-//                    console.log(res[0].price * itemBought + "  total price of items bought");
-//                    console.log(answers.selectId + "  checking id number");
-                    
-                    
-                    ///////////////////////////////////////////////////////////////////////////
-                    /// END testing app to check values of variables /////
-                    
-                    var totalPrice = res[0].price * itemBought;
-                    var itemPurchased = res[0].product;
-                    
-                    console.log(totalPrice + "  total price of items bought");
-                    
-                    connection.query(
-                        "UPDATE products SET ? WHERE ?", [
-                            {
-                                quanity: leftInStock
-                                
-                        },
-                            {
-                                id: answers.selectId
-                        }
+                  ]).then(function(answers) {
+        //set captured input as variables, pass variables as parameters.
+        var quantityAdded = answers.Quantity;
+        var IDOfProduct = answers.ID;
+        restockDatabase(IDOfProduct, quantityAdded);
+    });
+}; //end restockRequest
 
-                    ],
-                        function (error) {
-//                            console.log(price, amountBought);
-                            if (error) throw err;
-                            console.log("==============================================");
-                            console.log("\n\r");
-                            console.log("Order details:");
-                            console.log("Item(s) purchased: " + itemPurchased);
-                            console.log("Quanity purchased: " + itemBought + " @ $" + res[0].price);
-                            console.log("Total Cost: $" + totalPrice);
-                            console.log("\n\r");
-                            console.log("Thank you for shopping at-fell off the truck online-");
-                            console.log("==============================================");
-                            showProducts();
+        function restockDatabase(id, quant) {
+    //update the database
+    connection.query('SELECT * FROM products WHERE id = ' + id, function(error, response) {
+        if (error) { console.log(error) };
+        connection.query('UPDATE products SET quanity = Quanity + ' + quant + ' WHERE id = ' + id);
+        //re-run display to show updated results
+        showProducts();
+    });
+}; //end restockDatabase
 
-                        }
-                    );
-                } else {
-                    console.log("==============================================");
-                    console.log("\n\r");
-                    console.log("Not enough available, please choose a different quantity");
-                    console.log("\n\r");
-                    console.log("==============================================");
-                   showProducts();
-
-                }
-
+                //        
+                //        var query = "SELECT * FROM products WHERE ?";
+                //            connection.query(query, {
+                //                id: answers.selectId
+                //            }, function (err, res) {
+                //
+                //
+                //                // get the information of the chosen item, set input to variables, pass variables as Parameters
+                //
+                //                var inStock = res[0].quanity;
+                //                var itemBought = answers.amountBought;
+                //
+                //                if (inStock >= itemBought) {
+                //                    var leftInStock = inStock - itemBought;
+                //                    
+                //                    ///  testing app to check values of variables /////
+                //                    ///////////////////////////////////////////////////////////////////////////
+                //                    
+                ////                    
+                ////                    console.log(leftInStock + "  amount left in stock ******");
+                ////                    console.log(itemBought + "  amount bought");
+                ////                    console.log(res[0].price + "   price");
+                ////                    console.log(res[0].price * itemBought + "  total price of items bought");
+                ////                    console.log(answers.selectId + "  checking id number");
+                //                    
+                //                    
+                //                    ///////////////////////////////////////////////////////////////////////////
+                //                    /// END testing app to check values of variables /////
+                //                    
+                //                    var totalPrice = res[0].price * itemBought;
+                //                    var itemPurchased = res[0].product;
+                //                    
+                //                    console.log(totalPrice + "  total price of items bought");
+                //                    
+                //                    connection.query(
+                //                        "UPDATE products SET ? WHERE ?", [
+                //                            {
+                //                                quanity: leftInStock
+                //                                
+                //                        },
+                //                            {
+                //                                id: answers.selectId
+                //                        }
+                //
+                //                    ],
+                //                        function (error) {
+                ////                            console.log(price, amountBought);
+                //                            if (error) throw err;
+                //                            console.log("==============================================");
+                //                            console.log("\n\r");
+                //                            console.log("Order details:");
+                //                            console.log("Item(s) purchased: " + itemPurchased);
+                //                            console.log("Quanity purchased: " + itemBought + " @ $" + res[0].price);
+                //                            console.log("Total Cost: $" + totalPrice);
+                //                            console.log("\n\r");
+                //                            console.log("Thank you for shopping at-fell off the truck online-");
+                //                            console.log("==============================================");
+                //                            showProducts();
+                //
+                //                        }
+                //                    );
+                //                } else {
+                //                    console.log("==============================================");
+                //                    console.log("\n\r");
+                //                    console.log("Not enough available, please choose a different quantity");
+                //                    console.log("\n\r");
+                //                    console.log("==============================================");
+                //                   showProducts();
+                //
+                //                }
+                //
+                //            });
+                //        
+                //        });// inquier.prompt
             });
-        
-        });// inquier.prompt
-        });
-    }// conection.query
+    } // conection.query
